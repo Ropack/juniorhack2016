f361a25d6d799ccccf6ce93501db434f1f819c38
Ropack <ropik0@seznam.cz>
2016-12-02 19:48:13 +0100

    Add files via upload
    start

---
+++ test1/test1.ino
@@ -0,0 +1,25 @@
+// Pulse Monitor Test Script
+int sensorPin = 0;
+double alpha = 0.75;
+int period = 50;
+double change = 0.0;
+double minval = 0.0;
+void setup ()
+{
+  Serial.begin (9600);
+}
+void loop ()
+{
+    static double oldValue = 0;
+    static double oldChange = 0;
+ 
+    int rawValue = analogRead (sensorPin);
+    double value = alpha * oldValue + (1 - alpha) * rawValue;
+ 
+    Serial.print (rawValue);
+    Serial.print (";");
+    Serial.println(String((int)value) + "," + String((int)((value - (double)((int)value))*100)));
+    oldValue = value;
+ 
+    delay (period);
+}
